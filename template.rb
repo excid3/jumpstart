bf13e2215879a90850a23045aef63166f3f95eac
Chris Oliver <excid3@gmail.com>
2017-05-01 22:36:46 -0500

    Add Sidekiq Web UI that's locked down to admins only. Fixes #9

--- template.rb
+++ template.rb
@@ -59,6 +59,14 @@ end
 
 def add_sidekiq
   environment "config.active_job.queue_adapter = :sidekiq"
+
+  insert_into_file "config/routes.rb",
+    "require 'sidekiq/web'\n\n",
+    before: "Rails.application.routes.draw do"
+
+  insert_into_file "config/routes.rb",
+    "  authenticate :user, lambda { |u| u.admin? } do\n    mount Sidekiq::Web => '/sidekiq'\n  end\n\n",
+    after: "Rails.application.routes.draw do\n"
 end
 
 def add_foreman

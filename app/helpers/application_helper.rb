43306e27823401cbe7319ecf2378c9e5fe6244fc
Chris Oliver <excid3@gmail.com>
2017-07-21 12:25:45 -0500

    Add container and better control over notices

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,14 +7,4 @@ module ApplicationHelper
       notice: "alert-info"
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
-
-  def flash_messages(opts={})
-    flash.each do |msg_type, message|
-      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}") do
-        concat content_tag(:button, content_tag(:span, "&times;"), class: "close", data: { dismiss: 'alert' })
-        concat message
-      end)
-    end
-    nil
-  end
 end

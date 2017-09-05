9146494baed2345db132d34d16dce9c833fe7060
Scott Christensen <scott@codebyscott.io>
2017-09-06 00:44:34 +0900

    add name fields to devise registrations (#15)
    * add name fields to devise registrations
    
    * add name fields to devise registrations
    
    * added application controller to customize devise params

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    end
+end

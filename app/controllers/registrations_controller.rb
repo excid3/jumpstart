c1763059bc4bd9b49be835062169492c54ff37eb
codeByScott <scott@codebyscott.io>
2017-09-05 23:45:41 +0900

    add name fields to devise registrations

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,12 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+  end
+end

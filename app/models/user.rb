23d5ce4984a6b0071226c86a19be005e1326b126
Chris Oliver <excid3@gmail.com>
2017-10-13 11:02:42 -0500

    Add omniauthable. Fixes #18

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :masqueradable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :masqueradable, :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   validates :first_name, :last_name, presence: true
 

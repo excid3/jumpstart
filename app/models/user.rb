979f17a56649d497f7e55a2c0759da5f5468bfd8
Chris Oliver <excid3@gmail.com>
2017-10-13 14:59:48 -0500

    Include services on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  has_many :notifications, foreign_key: :recipient_id
-  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :masqueradable, :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+  has_many :notifications, foreign_key: :recipient_id
+  has_many :services
 
   validates :first_name, :last_name, presence: true
 

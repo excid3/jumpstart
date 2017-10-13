dcd6ae54a53f7a47e0bd5599ddc26b6b2178b0de
Chris Oliver <excid3@gmail.com>
2017-10-13 14:59:39 -0500

    Scope notifications controller to the current user

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,6 +2,6 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = Notification.where(recipient: current_user)
+    @notifications = current_user.notifications
   end
 end

9852dd790a4d84858df4562df92376231110bf40
Miguel Torres <migueltg93@gmail.com>
2017-11-07 16:41:40 +0100

    Grabs Stripe pk from secrets.yml

--- app/assets/javascripts/stripe.js
+++ app/assets/javascripts/stripe.js
@@ -1,4 +1,5 @@
-  var stripe = Stripe('your public stripe key');
+  var public_key = document.querySelector("meta[name='stripe-public-key']").content;
+  var stripe = Stripe(public_key);
   var elements = stripe.elements();
 
   // Create an instance of the card Element

0ff658c1f8e8641beba747363cf4c10c5e92a0d9
darkrubyist <darkrubyist@users.noreply.github.com>
2018-03-29 21:09:58 +0300

    Add unread notification scope.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,6 +2,8 @@ class Notification < ApplicationRecord
   belongs_to :recipient, class_name: "User"
   belongs_to :actor, class_name: "User"
   belongs_to :notifiable, polymorphic: true
+  
+  scope :unread, -> { where(read_at: nil) }
 
   def self.post(to:, from:, action:, notifiable:)
     recipients = Array.wrap(to)

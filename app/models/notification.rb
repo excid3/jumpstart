716ca1f51ce065386bd0202f018baa2d9e156927
darkrubyist <darkrubyist@users.noreply.github.com>
2018-03-30 17:55:23 +0300

    Add unread notification scope. (#41)
    * Add unread notification scope.
    
    * Add Recent Scope to Notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,6 +2,9 @@ class Notification < ApplicationRecord
   belongs_to :recipient, class_name: "User"
   belongs_to :actor, class_name: "User"
   belongs_to :notifiable, polymorphic: true
+  
+  scope :unread, -> { where(read_at: nil) }
+  scope :recent, -> { order(created_at: :desc) }.limit(5)
 
   def self.post(to:, from:, action:, notifiable:)
     recipients = Array.wrap(to)

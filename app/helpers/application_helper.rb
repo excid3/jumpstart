054e982bc7598d0b9494ada1ca73b9aae2c14de9
darkrubyist <darkrubyist@users.noreply.github.com>
2018-04-13 01:59:30 +0300

    Add Show SVG helper to handle svg icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,10 @@ module ApplicationHelper
       notice: "alert-info"
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
+  
+  def show_svg(path)
+    File.open("app/assets/images/icons/#{path}", "rb") do |file|
+      raw file.read
+    end
+  end
 end

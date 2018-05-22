aea6b4e0eef6f6ee247636ebbf1d1f3d8923f978
Chris Oliver <excid3@gmail.com>
2018-05-22 07:49:47 -0500

    Configure ActionCable to authenticate with Devise and use Redis by default

---
+++ app/channels/application_cable/connection.rb
@@ -0,0 +1,20 @@
+module ApplicationCable
+  class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+      logger.add_tags "ActionCable", "User #{current_user.id}"
+    end
+
+    protected
+
+      def find_verified_user
+        if current_user = env['warden'].user
+          current_user
+        else
+          reject_unauthorized_connection
+        end
+      end
+  end
+end

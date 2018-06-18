c5d3193ca4ef4830f97eaaeb3d49b5b615bcb2bc
Chris Oliver <excid3@gmail.com>
2018-06-18 09:23:38 -0500

    Update gems and add local_time

--- template.rb
+++ template.rb
@@ -27,24 +27,25 @@ end
 
 def add_gems
   gem 'administrate', '~> 0.10.0'
-  gem 'data-confirm-modal', '~> 1.6.2'
-  gem 'devise', '~> 4.4.3'
+  gem 'bootstrap', '~> 4.1', '>= 4.1.1'
+  gem 'data-confirm-modal', '~> 1.6', '>= 1.6.2'
+  gem 'devise', '~> 4.4', '>= 4.4.3'
   gem 'devise-bootstrapped', github: 'excid3/devise-bootstrapped', branch: 'bootstrap4'
-  gem 'devise_masquerade', '~> 0.6.0'
-  gem 'font-awesome-sass', '~> 4.7'
+  gem 'devise_masquerade', '~> 0.6.2'
+  gem 'font-awesome-sass', '~> 5.0', '>= 5.0.13'
+  gem 'foreman', '~> 0.84.0'
+  gem 'friendly_id', '~> 5.2', '>= 5.2.4'
   gem 'gravatar_image_tag', github: 'mdeering/gravatar_image_tag'
   gem 'jquery-rails', '~> 4.3.1'
-  gem 'bootstrap', '~> 4.0.0.beta'
+  gem 'local_time', '~> 2.0', '>= 2.0.1'
   gem 'mini_magick', '~> 4.8'
-  gem 'webpacker', '~> 3.4'
-  gem 'sidekiq', '~> 5.0'
-  gem 'foreman', '~> 0.84.0'
-  gem 'omniauth-facebook', '~> 4.0'
-  gem 'omniauth-twitter', '~> 1.4'
+  gem 'omniauth-facebook', '~> 5.0'
   gem 'omniauth-github', '~> 1.3'
-  gem 'whenever', require: false
-  gem 'friendly_id', '~> 5.1.0'
+  gem 'omniauth-twitter', '~> 1.4'
+  gem 'sidekiq', '~> 5.1', '>= 5.1.3'
   gem 'sitemap_generator', '~> 6.0', '>= 6.0.1'
+  gem 'webpacker', '~> 3.5', '>= 3.5.3'
+  gem 'whenever', require: false
 end
 
 def set_application_name
@@ -99,7 +100,7 @@ def add_bootstrap
   # Add Bootstrap JS
   insert_into_file(
     "app/assets/javascripts/application.js",
-    "\n//= require jquery\n//= require popper\n//= require bootstrap\n//= require data-confirm-modal",
+    "\n//= require jquery\n//= require popper\n//= require bootstrap\n//= require data-confirm-modal\n//= require local-time",
     after: "//= require rails-ujs"
   )
 end
@@ -227,7 +228,7 @@ after_bundle do
   add_notifications
   add_multiple_authentication
   add_friendly_id
-  
+
   copy_templates
 
   # Migrate

a3c9d9c3eb314c51444c18e4f50872bcb1e1d0c8
Chris Oliver <excid3@gmail.com>
2018-09-05 12:00:50 -0500

    Remove Devise trackable
    Devise removed trackable as a default to be more GDPR compliant. We don't have any GDPR stuff yet, so might as well remove it too. Fixes #58.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :masqueradable, :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  devise :masqueradable, :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable
 
   has_many :notifications, foreign_key: :recipient_id
   has_many :services

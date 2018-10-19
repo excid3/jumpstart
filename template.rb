012b2e89f07e500e13fda39b00d8c5069d4bfb82
Chris Oliver <excid3@gmail.com>
2018-10-19 11:32:47 -0500

    Fix password syntax

--- template.rb
+++ template.rb
@@ -155,11 +155,11 @@ def add_administrate
 
   gsub_file "app/dashboards/user_dashboard.rb",
     /email: Field::String/,
-    "email: Field::String,\n\t\tField::String.with_options(searchable: false),"
+    "email: Field::String,\n    password: Field::String.with_options(searchable: false)"
 
   gsub_file "app/dashboards/user_dashboard.rb",
     /FORM_ATTRIBUTES = \[/,
-    "FORM_ATTRIBUTES = [\n\t\t:password,"
+    "FORM_ATTRIBUTES = [\n    :password,"
 
   gsub_file "app/controllers/admin/application_controller.rb",
     /# TODO Add authentication logic here\./,

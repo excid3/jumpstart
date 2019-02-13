89f6f20ecb00478b62b56bbe7443bb6317c0bca0
Chris Oliver <excid3@gmail.com>
2019-02-13 13:49:50 -0600

    Update sidekiq

--- template.rb
+++ template.rb
@@ -43,7 +43,7 @@ def add_gems
   gem 'omniauth-facebook', '~> 5.0'
   gem 'omniauth-github', '~> 1.3'
   gem 'omniauth-twitter', '~> 1.4'
-  gem 'sidekiq', '~> 5.1', '>= 5.1.3'
+  gem 'sidekiq', '~> 5.2', '>= 5.2.5'
   gem 'sitemap_generator', '~> 6.0', '>= 6.0.1'
   gem 'webpacker', '~> 3.5', '>= 3.5.3'
   gem 'whenever', require: false

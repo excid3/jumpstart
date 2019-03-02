e74e4a9ee21e464b27522cff8942fa5fc857e3ce
Chris Oliver <excid3@gmail.com>
2019-03-01 23:46:24 -0600

    Add Javascript templates for webpacker

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,20 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("local-time").start()
+
+window.Rails = Rails
+
+import 'bootstrap'
+import 'data-confirm-modal'
+
+$(document).on("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})

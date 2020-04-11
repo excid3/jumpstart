64a4eb03feac8e10d4986875eba3ff7515b4f6d1
Chris Oliver <excid3@gmail.com>
2020-04-11 15:21:40 -0500

    Add test

---
+++ test/template_test.rb
@@ -0,0 +1,18 @@
+require "minitest/autorun"
+
+class TemplateTest < Minitest::Test
+  def setup
+    system("[ -d test_app ] && rm -rf test_app")
+  end
+
+  def teardown
+    setup
+  end
+
+  def test_generator_succeeds
+    output, err = capture_subprocess_io do
+      system("DISABLE_SPRING=1 rails new -m template.rb test_app")
+    end
+    assert_includes output, "Jumpstart app successfully created!"
+  end
+end

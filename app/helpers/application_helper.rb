a617868cf4849e5921fec8f2eb0ebda308321fda
Steve Polito <info@stevepolitodesign.com>
2020-05-20 14:24:44 -0400

    Add avatar helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,20 @@ module ApplicationHelper
       notice: "alert-info"
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
+  
+  def avatar_path(object, options = {size: 180, default: "mp", base: "https://secure.gravatar.com/avatar"})
+    size = options[:size]
+    default_image = options[:default]
+    base_url = options[:base]
+    base_url_params = "?s=#{size}&d=#{default_image}"
+    
+    if object.respond_to?(:avatar) && object.avatar.attached? && object.avatar.variable?
+      object.avatar.variant(resize_to_fill: [size, size, { gravity: 'Center' }])
+    elsif object.respond_to?(:email) && object.email
+      gravatar_id = Digest::MD5::hexdigest(object.email.downcase)
+      "#{base_url}/#{gravatar_id}#{base_url_params}"
+    else
+      "#{base_url}/00000000000000000000000000000000#{base_url_params}"
+    end
+  end
 end

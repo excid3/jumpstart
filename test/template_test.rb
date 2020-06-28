4034000f8f7b310201ad4bd6a8679cf41d1f74a1
Chris Oliver <excid3@gmail.com>
2020-06-28 12:39:48 -0500

    Skip git in CI tests

--- test/template_test.rb
+++ test/template_test.rb
@@ -11,7 +11,7 @@ class TemplateTest < Minitest::Test
 
   def test_generator_succeeds
     output, err = capture_subprocess_io do
-      system("DISABLE_SPRING=1 rails new -m template.rb test_app")
+      system("DISABLE_SPRING=1 SKIP_GIT=1 rails new -m template.rb test_app")
     end
     assert_includes output, "Jumpstart app successfully created!"
   end

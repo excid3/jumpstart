64fd7c7ccee59812786692c179e5150234f8e0c3
M. Benjamin Yusuf <B1G@users.noreply.github.com>
2021-03-01 18:13:17 +0100

    refactor navbar as bootstrap5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def is_active?(link_path)
+    current_page?(link_path) ? "active" : ""
+  end
+
   def bootstrap_class_for(flash_type)
     {
       success: "alert-success",

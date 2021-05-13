4cd364a824bec94b7d76bff81ecfc1b3b9876752
Chris Oliver <excid3@gmail.com>
2021-05-13 13:39:55 -0500

    Add new helpers

---
+++ app/helpers/avatar_helper.rb
@@ -0,0 +1,17 @@
+module AvatarHelper
+  def avatar_path(object, options = {})
+    size = options[:size] || 180
+    default_image = options[:default] || "mp"
+    base_url =  "https://secure.gravatar.com/avatar"
+    base_url_params = "?s=#{size}&d=#{default_image}"
+
+    if object.respond_to?(:avatar) && object.avatar.attached? && object.avatar.variable?
+      object.avatar.variant(resize_to_fill: [size, size, { gravity: 'Center' }])
+    elsif object.respond_to?(:email) && object.email
+      gravatar_id = Digest::MD5::hexdigest(object.email.downcase)
+      "#{base_url}/#{gravatar_id}#{base_url_params}"
+    else
+      "#{base_url}/00000000000000000000000000000000#{base_url_params}"
+    end
+  end
+end

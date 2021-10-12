e8660be907e31a8f90a004918d8d27011923a636
William Kennedy <williamkennedyjnr@gmail.com>
2021-10-12 14:14:23 +0100

    added back in devise line

--- template.rb
+++ template.rb
@@ -130,6 +130,8 @@ def add_users
   if Gem::Requirement.new("> 5.2").satisfied_by? rails_version
     gsub_file "config/initializers/devise.rb", /  # config.secret_key = .+/, "  config.secret_key = Rails.application.credentials.secret_key_base"
   end
+
+  inject_into_file("app/models/user.rb", "omniauthable, :", after: "devise :")
 end
 
 def add_authorization

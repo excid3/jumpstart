9837fef8382391657516b6769c378061895ea37f
David <davie668@gmail.com>
2021-10-22 17:00:21 +0100

    Fixes build script in package.json
    Before the `build:css` command was being overwritten by the esbuild script.
    Now the `build` script will point to the esbuild script, leaving the `build:css` command to build the CSS assets.

--- template.rb
+++ template.rb
@@ -247,7 +247,7 @@ def add_esbuild_script
   if (`npx -v`.to_f < 7.1 rescue "Missing")
     say %(Add "scripts": { "build": "#{build_script}" } to your package.json), :green
   else
-    run %(npm set-script build:css "#{build_script}")
+    run %(npm set-script build "#{build_script}")
   end
 end
 

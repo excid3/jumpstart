70fe12e57b9d9c8ed404ed0c2c1013faaa3edc4e
Moses Gathuku <mosesgathuku95@gmail.com>
2021-10-22 22:45:54 +0300

    import all channels in channel index

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,5 +1,4 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
 
-const channels = require.context('.', true, /_channel\.js$/)
-channels.keys().forEach(channels)
+import "./**/*_channel.js"

3d9c2bde9e3c8a532a87900aec7016126c3404ea
Andy Waite <andyw8@users.noreply.github.com>
2022-02-28 20:55:26 -0500

    Fix CI testing for mysql and postgres

--- test/template_test.rb
+++ test/template_test.rb
@@ -3,32 +3,34 @@ require "minitest/autorun"
 class TemplateTest < Minitest::Test
   def setup
     system("[ -d test_app ] && rm -rf test_app")
+    system("git config --global init.defaultBranch main")
   end
 
   def teardown
     setup
   end
 
-  def test_generator_succeeds
+  def test_generator_succeeds_with_default_database
     output, err = capture_subprocess_io do
       system("DISABLE_SPRING=1 SKIP_GIT=1 rails new test_app -m template.rb")
     end
+    assert_nil err
     assert_includes output, "Jumpstart app successfully created!"
   end
 
-  # TODO: Fix these tests on CI so they don't fail on db:create
-  #
-  # def test_generator_with_postgres_succeeds
-  #   output, err = capture_subprocess_io do
-  #     system("DISABLE_SPRING=1 SKIP_GIT=1 rails new test_app -m template.rb -d postgresql")
-  #   end
-  #   assert_includes output, "Jumpstart app successfully created!"
-  # end
+  def test_generator_with_postgres_succeeds
+    output, err = capture_subprocess_io do
+      system("DISABLE_SPRING=1 SKIP_GIT=1 rails new test_app -m template.rb -d postgresql")
+    end
+    assert_nil err
+    assert_includes output, "Jumpstart app successfully created!"
+  end
 
-  # def test_generator_with_mysql_succeeds
-  #   output, err = capture_subprocess_io do
-  #     system("DISABLE_SPRING=1 SKIP_GIT=1 rails new test_app -m template.rb -d mysql")
-  #   end
-  #   assert_includes output, "Jumpstart app successfully created!"
-  # end
+  def test_generator_with_mysql_succeeds
+    output, err = capture_subprocess_io do
+      system("DISABLE_SPRING=1 SKIP_GIT=1 rails new test_app -m template.rb -d mysql")
+    end
+    assert_nil err
+    assert_includes output, "Jumpstart app successfully created!"
+  end
 end

f49561d5c33493a65e2700f2e4768f6f87bb68df
Fig <fig@users.noreply.github.com>
2023-11-07 03:12:58 +0000

    Relax watchDirectories glob
    The previous glob was a little too restrictive and would not watch files target at request variants e.g. `app/views/things/show.html+phone.erb`

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -22,7 +22,7 @@ const entryPoints = [
 ]
 const watchDirectories = [
   "./app/javascript/**/*.js",
-  "./app/views/**/*.html.erb",
+  "./app/views/**/*.html*.erb",
   "./app/assets/builds/**/*.css", // Wait for cssbundling changes
 ]
 const config = {
